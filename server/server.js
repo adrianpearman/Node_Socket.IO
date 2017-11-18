1586e611151b74392e8b5adf247c38b3305ebf51
Adrian Pearman <adrianpearman12@gmail.com>
2017-11-18 16:58:03 -0500

    socket.io npm module added to the application. server.js file updated to include connected and disconnected options from socket.io

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
 const path = require('path');
-const express = require('express')
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public')
-const app = express()
-const PORT = process.env.PORT || 3000
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+let server = http.createServer(app);
+let io = socketIO(server);
 
 console.log(__dirname + '/../public');
 console.log(publicPath);
 
-app.use(express.static(publicPath))
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-// app.get('/', (req, res) => {
-//   res.render('/public/index.html')
-// })
-
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  })
 })
+
+app.use(express.static(publicPath));
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
