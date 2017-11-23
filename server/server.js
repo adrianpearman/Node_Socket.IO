244182e699db3b8eb8cda0574137a09e007dad7c
Adrian Pearman <adrianpearman12@gmail.com>
2017-11-22 21:49:34 -0500

    socket emitting functionality added.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,23 @@ console.log(publicPath);
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  //send a request back to the user with an object
+  // socket.emit('newEmail', {
+  //   from: 'adrianpearman12@gmail.com',
+  //   text: 'hey!',
+  //   timeStamp: 1287
+  // })
+
+  socket.on('createMessage', (message) => {
+    console.log(message);
+    //this will emit it to the browser anot the console
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  })
+
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   })
