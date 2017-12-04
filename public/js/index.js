5c6f8cbe03e36b1215c92e55d7f22b502070c7ee
Adrian Pearman <adrianpearman12@gmail.com>
2017-12-03 19:44:33 -0500

    updated index.js to include auto scroll feature.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  let messages = $('#messages-list')
+  let newMessage = messages.children('li:last-child')
+  // Heights
+  let clientHeight = messages.prop('clientHeight')
+  let scrollTop = messages.prop('scrollTop')
+  let scrollHeight = messages.prop('scrollHeight')
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight()
+
+  if ((clientHeight + scrollTop + newMessageHeight + lastMessageHeight) >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function(){
   console.log('connected to server');
 
@@ -20,6 +36,7 @@ socket.on('newMessage', function(message){
   })
 
   $('#messages-list').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function(message){
@@ -32,6 +49,7 @@ socket.on('newLocationMessage', function(message){
   })
 
   $('#messages-list').append(li)
+  scrollToBottom()
 })
 
 
